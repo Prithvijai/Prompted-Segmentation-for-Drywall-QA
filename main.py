a66e58be030902cfe8d46c35ee12bd99f057a6e3
Prithvijai <prithvijai0510@gmail.com>
2026-02-02 23:40:10 -0700

    test: masks on Drywall and cracks data

--- main.py
+++ main.py
@@ -8,12 +8,18 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    d = DrywallQADatasetCustom("/home/saitama/Documents/Prompted Segmentation for Drywall QA/datasets/Drywall_Join_Detect_v1/train/")
-    # print(len(d))
-    print(d[0])    
-    image = d[0]['mask']
+    d1_train = DrywallQADatasetCustom("/home/saitama/Documents/Prompted Segmentation for Drywall QA/datasets/Drywall_Join_Detect_v1/train/")
+    d2_train = DrywallQADatasetCustom("/home/saitama/Documents/Prompted Segmentation for Drywall QA/datasets/cracks_v1/train")
+    # print(len(d))   
+    image1, mask1 = d1_train[0]['image'], d1_train[0]['mask']
+    image2, mask2 = d2_train[0]['image'], d2_train[0]['mask']
     # print(annotations)
-    plt.imshow(image)
+    fig, ax = plt.subplots(2, 2, figsize=(12, 12))
+    ax = ax.flatten()
+    ax[0].imshow(image1)
+    ax[1].imshow(image2)
+    ax[2].imshow(mask1)
+    ax[3].imshow(mask2)
     plt.show()
     # d.data_info()
 
